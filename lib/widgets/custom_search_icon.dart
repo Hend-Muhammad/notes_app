920720235b67a2630399755412a39ca8751f1412
Hend-Mohamad <hend.mohamad105@gmail.com>
2025-05-13 13:07:38 +0300

    create app bar

---
+++ lib/widgets/custom_search_icon.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchIcon extends StatelessWidget {
+  const CustomSearchIcon({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Container(
+        width: 46,
+        height: 46,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Colors.white.withValues(alpha: 0.10),
+        ),
+        child:  Icon(Icons.search, size: 28,),
+      ),
+    );
+  }
+}

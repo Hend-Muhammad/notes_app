149e654c0fefffdeb4a0b286226073bd314af89f
Hend-Mohamad <hend.mohamad105@gmail.com>
2025-05-15 04:08:27 +0300

    Edit note function

--- lib/widgets/custom_search_icon.dart
+++ lib/widgets/custom_search_icon.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
 
 class CustomSearchIcon extends StatelessWidget {
-  CustomSearchIcon({
+  const CustomSearchIcon({
     super.key,
-    required this.icon
+    required this.icon,
+    this.onPressed
   });
-  IconData icon;
+  final IconData icon;
+  final void Function()? onPressed;
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      child: Container(
-        width: 46,
-        height: 46,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          color: Colors.white.withValues(alpha: 0.10),
-        ),
-        child:  Icon(icon, size: 28,),
+    return Container(
+      width: 46,
+      height: 46,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.white.withValues(alpha: 0.10),
       ),
+      child:  IconButton(
+        onPressed: onPressed,
+        icon: Icon(icon),
+        iconSize: 28),
     );
   }
 }

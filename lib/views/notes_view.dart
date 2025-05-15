8a18751f4a565bc984f3adb436023d0cb1261c78
Hend-Mohamad <hend.mohamad105@gmail.com>
2025-05-15 04:41:45 +0300

    create color list view

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/constant.dart';
-import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/widgets/add_note_bottom_sheet.dart';
-import 'package:notes_app/widgets/custom_app_bar.dart';
-import 'package:notes_app/widgets/notes_list_view.dart';
+import 'package:notes_app/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
@@ -27,37 +24,6 @@ class NotesView extends StatelessWidget {
         child: const Icon(Icons.add, color: Colors.black),
       ),
       body: NotesViewBody(),
-    );
-  }
-}
-
-class NotesViewBody extends StatefulWidget {
-  const NotesViewBody({
-    super.key,
-  });
-
-  @override
-  State<NotesViewBody> createState() => _NotesViewBodyState();
-}
-
-class _NotesViewBodyState extends State<NotesViewBody> {
-  @override
-  void initState() {
-    BlocProvider.of<NotesCubit>(context).fetchAllNotes();
-    super.initState();
-  }
-  
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: Column(
-        children: [
-          SizedBox(height: 50),
-          CustomAppBar(title: 'Notes', icon: Icons.search),
-          Expanded(child: NotesListView()),
-        ],
-      ),
     );
   }
 }
---
+++ lib/widgets/colors_list_view.dart
@@ -0,0 +1,70 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+class ColorItem extends StatelessWidget {
+  const ColorItem({super.key, required this.isActive, required this.color});
+  final bool isActive;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return isActive? 
+    CircleAvatar(
+      radius: 38,
+        backgroundColor: Colors.white,
+      child: CircleAvatar(
+        radius: 36,
+        backgroundColor: color,
+      ),
+    ) :
+    CircleAvatar(
+      radius: 38,
+      backgroundColor: color,
+    );
+  }
+}
+
+class ColorsListView extends StatefulWidget {
+  const ColorsListView({super.key});
+
+  @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  
+  int currentIndex = 0;
+  List<Color> colors = [
+    Color(0xff776D5A),
+    Color(0xff987D7C),
+    Color(0xffA09CB0),
+    Color(0xffA3B9C9),
+    Color(0xffABDAE1),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 38*2,
+      child: ListView.builder(
+        itemCount: colors.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 6),
+            child:  GestureDetector(
+              onTap: (){
+                currentIndex = index;
+                setState(() {});
+              },
+              child: ColorItem(
+                isActive: currentIndex == index ,
+                color: colors[index],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
